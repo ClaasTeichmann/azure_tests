6cf65f816df31d04d60a3f5e7f0ca76745285468
Claas Teichmann <claas.teichmann@hzg.de>
2022-03-11 22:32:08 +0100

    Updated skeleton.

--- azure_cli_scripts/variables_skeleton.sh
+++ azure_cli_scripts/variables_skeleton.sh
@@ -4,16 +4,16 @@
 # 
 
 # ID of your Azure subscription
-subscription_id=""
+export subscription_id=""
 # Name of the resource group
-batch_rg=""
+export batch_rg=""
 # Region
-region="westeurope"
+export region="westeurope"
 # Unique batch account name (e.g. batchwsaccount3245353)
-batch_account_name=""
+export batch_account_name=""
 # Unique extension for the keyvault name (e.g. kv234)
 # Maxc 24 Zeichen für  ${batch_account_name}${keyvault_extension} 
-keyvault_extension="kv18"
+export keyvault_extension="kv18"
 
 
 # Die restlichen Parameter sind Variabel, sollten aber einer
@@ -21,24 +21,46 @@ keyvault_extension="kv18"
 # Die Parameter sind Beispiele, wie ich sie verwendet habe
 
 # Unique storage account name (e.g. batchwastorage3245353)
-storage_account_name=""
+export storage_account_name=""
 # "batchwasest3245353"
 
 # VNET name
-batch_vnet_name=""
+export batch_vnet_name=""
 # "batch-sest-vnet"
 
 # Compute subnet name
-compute_subnet_name=""
+export compute_subnet_name=""
 # "compute"
 
 # Unique storage account name for shared Azure Files NFS storage (e.g. batchwsnfsstorage3422435234)
-nfs_storage_account_name="" 
+export nfs_storage_account_name="" 
 # "batchsestnfsa37"
 
 # Name of tha Azure Files fileshare
-nfs_share="shared"
+export nfs_share="shared"
 
 # Name des Pools
-pool_id="batch-ws2-pool"
+# ws2 for small machines
+# ws3 for big machines
+export pool_id="batch-ws2-pool"
+
 # Wuerde ich hier definieren
+
+# Public key for testing purposes
+export ssh_pubkey="ssh-rsa AAAAB....."
+
+# Type of the VM to be created
+# For paths
+#export VM_TYPE="HB120rsv2"
+# For init-script
+#export POOL_VM_SIZE="Standard_HB120rs_v2"
+
+# For paths
+export VM_TYPE="STANDARD_F2s"
+# For init-script
+export POOL_VM_SIZE="STANDARD_F2s_v2"
+
+
+# Number of VMs to be created
+export VM_NUMBER=2
+
